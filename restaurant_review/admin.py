89f9bb40b03ea13f0654ff73a52e9826df61d974
jomoglobal <josephmontague@gmail.com>
2024-01-31 17:17:43 -0800

    made change to admin.py

--- restaurant_review/admin.py
+++ restaurant_review/admin.py
@@ -1,8 +1,18 @@
+#ORIGINAL FILE:
+# from django.contrib import admin
+
+# from .models import Restaurant, Review
+
+# # Register your models here.
+
+# admin.site.register(Restaurant)
+# admin.site.register(Review)
+
+
+#UPDATED FILE:
+# restaurant_review/admin.py
+
 from django.contrib import admin
+from .models import AzurePricing
 
-from .models import Restaurant, Review
-
-# Register your models here.
-
-admin.site.register(Restaurant)
-admin.site.register(Review)
+admin.site.register(AzurePricing)
